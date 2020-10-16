92527b8b5deaf878d71f42bf294d8b0ebe1702e9
abdo <babdo6806@gamil.com>
2020-10-16 23:36:03 +0200

    modifying

--- app/src/main/java/com/example/itiprojecte_commerce/MainActivity.java
+++ app/src/main/java/com/example/itiprojecte_commerce/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.itiprojecte_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button join_btn,login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        join_btn=findViewById(R.id.join_btn);
+        login_btn=findViewById(R.id.login_btn);
+        join_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, login_page.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/itiprojecte_commerce/login_page.java
@@ -0,0 +1,34 @@
+package com.example.itiprojecte_commerce;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class login_page extends Fragment {
+
+
+
+    public login_page() {
+
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.login_page, container, false);
+    }
+}
