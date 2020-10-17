8b8e7daa78f51f81991448c391cdc9a212e5252f
abdo <babdo6806@gamil.com>
2020-10-17 15:43:16 +0200

    newf

--- app/src/main/java/com/example/itiprojecte_commerce/MainActivity.java
+++ app/src/main/java/com/example/itiprojecte_commerce/MainActivity.java
@@ -9,20 +9,13 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button join_btn,login_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        join_btn=findViewById(R.id.join_btn);
-        login_btn=findViewById(R.id.login_btn);
-        join_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, login_page.class);
-                startActivity(i);
-            }
-        });
+
+
     }
 }
--- app/src/main/java/com/example/itiprojecte_commerce/main_frag.java
+++ app/src/main/java/com/example/itiprojecte_commerce/main_frag.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 
 public class main_frag extends Fragment {
@@ -27,6 +30,19 @@ public class main_frag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.main_frag, container, false);
+       View v =  inflater.inflate(R.layout.main_frag, container, false);
+        Button login_btn = v.findViewById(R.id.login_btn);
+        final NavController navController = Navigation.findNavController(v);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_main_frag_to_loginFrag);
+
+            }
+        });
+
+
+       return v;
     }
 }
