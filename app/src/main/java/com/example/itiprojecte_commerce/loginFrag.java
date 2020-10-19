bd421aa458b42ba1679158792e5059d52b3b9d0a
abdo <babdo6806@gamil.com>
2020-10-19 18:27:31 +0200

    login

--- app/src/main/java/com/example/itiprojecte_commerce/loginFrag.java
+++ app/src/main/java/com/example/itiprojecte_commerce/loginFrag.java
@@ -2,6 +2,8 @@ package com.example.itiprojecte_commerce;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -28,7 +30,18 @@ public class loginFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+
+
+        View v =  inflater.inflate(R.layout.fragment_login, container, false);
+
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
